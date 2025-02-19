47b68206afec265634070f0e5c717966a3f61ddb
Harishankar Priyadarshi <harishankarkumar2p@gmail.com>
2025-02-19 17:40:32 +0530

    captain authentication done

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,10 @@ import express, { json } from "express"
 import cors from 'cors'
 import morgan from 'morgan';
 import connectToDb from './db/db.js';
-connectToDb()
-import userRoutes from "./routes/user.routes.js"
 import cookieParser from 'cookie-parser';
-
+import userRoutes from "./routes/user.routes.js"
+import captainRoutes from "./routes/captain.routes.js"
+connectToDb()
 const app = express();
 
 
@@ -27,6 +27,7 @@ app.get('/', (req, res) => {
 
 // routes defined here
 app.use('/users', userRoutes)
+app.use('/captain', captainRoutes)
 
 
 export default app;
--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken'
 import userModel from '../models/user.model.js'
 import blackListTokenModel from '../models/blacklistToken.model.js'
+import captainModel from '../models/captain.model.js'
 
 export const authUser = async (req, res, next) => {
     const token = req.cookies.token || req.headers.authorization?.split(' ')[1]; // authorizationtoken will be send with `bearer ${token}`"
@@ -27,5 +28,34 @@ export const authUser = async (req, res, next) => {
 
     } catch (err) {
         return res.status(401).json({ message: 'Unauthorized' });
+    }
+}
+
+export const authCaptain = async (req, res, next) => {
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+
+
+    if (!token) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    const isBlacklisted = await blackListTokenModel.findOne({ token: token });
+
+
+
+    if (isBlacklisted) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const captain = await captainModel.findById(decoded._id)
+        req.captain = captain;
+
+        return next()
+    } catch (err) {
+        console.log(err);
+
+        res.status(401).json({ message: 'Unauthorized' });
     }
 }
